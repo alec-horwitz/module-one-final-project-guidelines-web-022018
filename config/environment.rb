1be74039ec9f10483c7140a8660eb81aab3e0d0e
alec-horwitz <horwitz_alec@wheatoncollege.edu>
2018-03-06 16:13:06 -0500

    started_it

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 require 'bundler'
 Bundler.require
 
+require 'active_record'
+
+Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each{ |f| require f }
+
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
 require_all 'lib'
